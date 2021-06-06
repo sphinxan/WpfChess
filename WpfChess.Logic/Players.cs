556c652a49a54ce791bafcb20b4f33d3f2f39572
) <dazaj.osamu@mail.ru>
2021-06-06 23:52:19 +0500

    добавлена логика

---
+++ WpfChess.Logic/Players.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfChess.Logic
+{
+    public class FirstPlayer
+    {
+        public static string Name { get; set; }
+        public static int Wins { get; set; }
+
+        public FirstPlayer(string name, int wins)
+        {
+            Name = name;
+            Wins = wins;
+        }
+    }
+    public class SecondPlayer
+    {
+        public static string Name { get; set; }
+        public static int Wins { get; set; }
+
+        public SecondPlayer(string name, int wins)
+        {
+            Name = name;
+            Wins = wins;
+        }
+    }
+}
